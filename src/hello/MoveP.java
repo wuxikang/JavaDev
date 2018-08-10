90385ea5098e19671d89defcc15ab9d7dfcf1123
I079889 <andy.wu01@sap.com>
2018-08-10 15:50:13 +0800

    Add new lib

---
+++ src/hello/MoveP.java
@@ -0,0 +1,22 @@
+package hello;
+
+public class MoveP {
+
+	public int[] movePelements(int[] arr, int num) {
+		int[] temparr = new int[num];
+		int i = 0;
+		while (i < num) {
+			temparr[i] = arr[i];
+			i++;
+		}
+		for (int j = 0; j < arr.length - num; j++) {
+			arr[j] = arr[j + num];
+		}
+		for (int k = 0; k < num; k++) {
+			arr[arr.length - num + k] = temparr[k];
+		}
+		return arr;
+
+	}
+
+}
