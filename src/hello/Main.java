feb7f16693bfcefc91cad7ffde73bfbd7a58f17b
I079889 <andy.wu01@sap.com>
2018-08-10 15:49:36 +0800

    add new test

--- src/hello/Main.java
+++ src/hello/Main.java
@@ -9,29 +9,45 @@ import java.nio.charset.Charset;
 
 public class Main {
 
-	static int count=0;
+	static int count = 0;
+
 	/**
 	 * Main
+	 * 
 	 * @param args
 	 */
 	public static void main(String[] args) {
-		
-		String filepath="/Users/i079889/Documents/workspace/ASEDOJO/src/hello/Main.java";
-		System.out.println("Code lines of current file is "+getCodeLinesOfSource(filepath));
-		filepath="/Users/i079889/Documents/workspace/ASEDOJO/src/hello/whitespaceandcommentstest.java";
-		System.out.println("Code lines of "+"whitespaceandcommentstest.java is "+getCodeLinesOfSource(filepath));
-		filepath="/Users/i079889/Documents/workspace/ASEDOJO/src/hello/Emptyfiletest.java";
-		System.out.println("Code lines of "+"Emptyfiletest.java is "+getCodeLinesOfSource(filepath));
-		
-        File path = new File("/Users/i079889/git/gradle-aafsystem-module/src/main/java/com/successfactors/aaf/tests/system/odata");
-        search(path);
-        
-        System.out.println(count+" java files so far!");
+
+		// String
+		// filepath="/Users/i079889/Documents/workspace/ASEDOJO/src/hello/Main.java";
+		// System.out.println("Code lines of current file is
+		// "+getCodeLinesOfSource(filepath));
+		// filepath="/Users/i079889/Documents/workspace/ASEDOJO/src/hello/whitespaceandcommentstest.java";
+		// System.out.println("Code lines of "+"whitespaceandcommentstest.java is
+		// "+getCodeLinesOfSource(filepath));
+		// filepath="/Users/i079889/Documents/workspace/ASEDOJO/src/hello/Emptyfiletest.java";
+		// System.out.println("Code lines of "+"Emptyfiletest.java is
+		// "+getCodeLinesOfSource(filepath));
+		//
+		// File path = new
+		// File("/Users/i079889/git/gradle-aafsystem-module/src/main/java/com/successfactors/aaf/tests/system/odata");
+		// search(path);
+		//
+		// System.out.println(count+" java files so far!");
+		int[] testarr = { 1, 3, 5, 7, 9 };
+		MoveP test = new MoveP();
+		test.movePelements(testarr, 3);
+		int i = 0;
+		while (i < testarr.length) {
+			System.out.println(testarr[i]);
+			i++;
+		}
 	}
 
-	//this is for testing
+	// this is for testing
 	/**
 	 * Main method to test
+	 * 
 	 * @param filepath
 	 * @return
 	 */
@@ -67,41 +83,41 @@ public class Main {
 		} catch (Exception e) {
 			System.out.println(e);
 		}
-		
+
 		return count;
 	}
-	
-    //recursive search all .java files
-    private static void search(File path) {
-        File[] files = path.listFiles();
-         
-        //search all .java files in the directory,
-        //but not include those .java files occurs in the sub-directory
-        if(path.getAbsolutePath().equalsIgnoreCase("/Users/i079889/git/gradle-aafsystem-module/src/main/java/com/successfactors/aaf/tests/system/odata") ){
-            listFilteredFileName(path);
-        }
-         
-        //search all .java files in the sub-directory
-        for (File file : files) {
-            if (file.isDirectory()) {
-            	if(file.getName().endsWith("sapui5support")||file.getName().endsWith("metadatadriven"))
-            	{
-            		continue;
-            	}
-                //File dir = file.getAbsoluteFile();
-                listFilteredFileName(file);
-                search(file);
-            }
-        }
-    }
- 
-    //show .java files by directory
-    private static void listFilteredFileName(File file) {
-        String[] files = file.list(new DirFilter("\\w+.java"));
-        for (String fileName: files) {
-            System.out.println(fileName + " ");
-            count++;
-        }
-    }
+
+	// recursive search all .java files
+	private static void search(File path) {
+		File[] files = path.listFiles();
+
+		// search all .java files in the directory,
+		// but not include those .java files occurs in the sub-directory
+		if (path.getAbsolutePath().equalsIgnoreCase(
+				"/Users/i079889/git/gradle-aafsystem-module/src/main/java/com/successfactors/aaf/tests/system/odata")) {
+			listFilteredFileName(path);
+		}
+
+		// search all .java files in the sub-directory
+		for (File file : files) {
+			if (file.isDirectory()) {
+				if (file.getName().endsWith("sapui5support") || file.getName().endsWith("metadatadriven")) {
+					continue;
+				}
+				// File dir = file.getAbsoluteFile();
+				listFilteredFileName(file);
+				search(file);
+			}
+		}
+	}
+
+	// show .java files by directory
+	private static void listFilteredFileName(File file) {
+		String[] files = file.list(new DirFilter("\\w+.java"));
+		for (String fileName : files) {
+			System.out.println(fileName + " ");
+			count++;
+		}
+	}
 
 }
